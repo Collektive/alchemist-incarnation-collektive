6c4d3a5027655e026361098baec90bde5295e31d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-10-29 17:08:12 +0100

    feat: use new collektive api

--- src/main/kotlin/it/unibo/collektive/alchemist/device/CollektiveDevice.kt
+++ src/main/kotlin/it/unibo/collektive/alchemist/device/CollektiveDevice.kt
@@ -8,8 +8,12 @@ import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.Time
 import it.unibo.collektive.ID
 import it.unibo.collektive.IntId
-import it.unibo.collektive.Network
 import it.unibo.collektive.field.Field
+import it.unibo.collektive.messages.AnisotropicMessage
+import it.unibo.collektive.messages.InboundMessage
+import it.unibo.collektive.messages.IsotropicMessage
+import it.unibo.collektive.messages.OutboundMessage
+import it.unibo.collektive.networking.Network
 import it.unibo.collektive.stack.Path
 
 /**
@@ -38,16 +42,30 @@ class CollektiveDevice<P>(
 
     override fun cloneOnNewNode(node: Node<Any>) = TODO()
 
-    override fun send(localId: ID, message: Map<Path, *>) {
-        environment.getNeighborhood(node)
-            .mapNotNull { it.asPropertyOrNull<Any, CollektiveDevice<P>>() }
-            .forEach { it.receiveMessage(currentTime, localId, message) }
+    override fun read(): Set<InboundMessage> {
+        return validMessages
+            .filter { (_, value) -> value.first + retainMessagesFor >= currentTime }
+            .also { validMessages = it }
+            .mapValues { (id, value) -> InboundMessage(id, value.second) }
+            .values
+            .toSet()
     }
 
-    override fun receive(): Map<ID, Map<Path, *>> = validMessages
-        .filter { (_, value) -> value.first + retainMessagesFor >= currentTime }
-        .also { validMessages = it }
-        .mapValues { (_, value) -> value.second }
+    override fun write(messages: Set<OutboundMessage>) {
+        messages.forEach { message ->
+            when (message) {
+                is IsotropicMessage ->
+                    environment.getNeighborhood(node)
+                        .mapNotNull { it.asPropertyOrNull<Any, CollektiveDevice<P>>() }
+                        .forEach { it.receiveMessage(currentTime, message.senderId, message.message) }
+                is AnisotropicMessage ->
+                    environment.getNeighborhood(node)
+                        .mapNotNull { it.asPropertyOrNull<Any, CollektiveDevice<P>>() }
+                        .firstOrNull { it.node.id == (message.receiverId as IntId).id }
+                        ?.also { it.receiveMessage(currentTime, message.senderId, message.message) }
+            }
+        }
+    }
 
     override fun distances(): Field<Double> {
         val res: Map<ID, Double> = mapOf(IntId(node.id) to 0.0) +
